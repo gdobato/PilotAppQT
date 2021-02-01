8b77396f0a4d1b879d50d7e14c40b2e5b092db8f
Gabriel Dobato <dobatog@gmail.com>
2021-02-01 23:29:05 +0000

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -22,13 +22,9 @@ MainWindow::~MainWindow()
 void MainWindow::on_pushButton_clicked()
 {
 
-    //QCoreApplication::exit();
+
     QProcess *process = new QProcess(this);
-    //QString file = QDir::homePath + "/holamundo.out";
-    //process->execute("./home/holamundo.out");
-   // process->start("/home/jenifer/build-Bike-Desktop_Qt_5_7_0_GCC_64bit-Release (copia)/Bike");
-   // process->start("/home/jenifer/build-Bike-Desktop_Qt_5_7_0_GCC_64bit-Release/Bike");
-   //process->start("../Bike/externalApp/Bike");
+
     process->start(EXTERNALAPPPATH);
 
     if (process->waitForStarted() == false) {
@@ -46,5 +42,5 @@ void MainWindow::on_pushButton_clicked()
         //process->waitForReadyRead();
 
       //  qDebug() << process->readAllStandardOutput();
-    //process->execute("/home/holamundo.out");
+      //process->execute("/home/holamundo.out");
 }
